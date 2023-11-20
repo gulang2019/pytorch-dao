f4dba3ebabfb6d54ac66f448d5e637e3d0cf2288
Zelong Guan <zlguan@zju.edu.cn>
2023-11-20 12:33:55 -0500

    Update torch add test

--- dao/testing/add.py
+++ dao/testing/add.py
@@ -2,11 +2,20 @@ import dao
 
 import torch
 
+dao.launch()
+
+assert torch.cuda.is_available()
+device = torch.device("cuda")
+
 # Create two tensors 'a' and 'b'
-a = torch.tensor([1.0, 2.0, 3.0])
-b = torch.tensor([4.0, 5.0, 6.0])
+a = torch.tensor([1.0, 2.0, 3.0], device=device)
+b = torch.tensor([4.0, 5.0, 6.0], device=device)
 
 # Perform element-wise addition
 result = a + b
 
+dao.sync()
 print(result)
+
+result_cpu = result.cpu()
+print(result_cpu)
