61548e98f76ed6a3f64dd9d74473c63e7eca992e
gulang2019 <siyuanc3@andrew.cmu.edu>
2023-11-21 22:27:12 -0500

    add stub

---
+++ dao/test/stub/ATen/Scalar.h
@@ -0,0 +1,10 @@
+#ifndef ATEN_SCALAR_H_
+#define ATEN_SCALAR_H_ 
+
+namespace at {
+class Scalar{
+    bool defined() const {return true;}
+}; 
+} // namespace at 
+
+#endif // ATEN_SCALAR_H_
--- dao/test/stub/ATen/Tensor.h
+++ dao/test/stub/ATen/Tensor.h
@@ -1,9 +1,16 @@
 #ifndef ATEN_TENSOR_H_
 #define ATEN_TENSOR_H_ 
 
+#include <string>
+
 namespace at {
 class Tensor{
+public:
     bool defined() const {return true;}
+    std::string toString() const {return "Tensor";}
+    std::string name() const {return "Tensor";}
+    int sizes() const {return 0;}
+    int use_count() const {return 0;}
 }; 
 } // namespace at 
 
